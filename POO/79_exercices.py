f893672376ba45aa786cc0140032b8bcb5225dfd
Arthur Guilherme <104983447+devarthurguilherme@users.noreply.github.com>
2025-02-21 20:44:15 +0000

    Update exercice

--- POO/79_exercices.py
+++ POO/79_exercices.py
@@ -6,12 +6,12 @@ class ListManipulator:
     # Check Number
     def checkNumber(self):
         while True:
-            userInput = input("Digit a integer number: ")
+            userInput = input("Digit a number: ")
             try:
                 self.userNumber = int(userInput)
                 return self.userNumber
             except ValueError:
-                print("\nPlease, digit a valid number!\n")
+                print("\nPlease, digit a integer valid number!\n")
 
     # Show  List
     def showList(self):
@@ -23,39 +23,99 @@ class ListManipulator:
 
     # Add Element
     def addListElement(self):
-        self.userNumber = self.checkNumber()
-        self.myList.append(self.userNumber)
-        self.showList()
+        print("Let's add a element\n")
+        while True:
+            self.userNumber = self.checkNumber()
+            if self.userNumber not in self.myList:
+                self.myList.append(self.userNumber)
+                break
+            else:
+                print("\nDigit a number that doesn't exist in the list!\n")
 
     # Remove Element
     def removeElement(self):
-        self.userNumber = self.checkNumber()
+        print("Let's remove a element\n")
+        if not self.myList:
+            print("Empty list")
+            return
         while True:
+            print("\nDigit a integer element to remove\n")
+            self.userNumber = self.checkNumber()
             try:
                 self.myList.remove(self.userNumber)
-                self.showList()
                 break
             except ValueError:
-                print(f"{self.userNumber} doesn't exist!\n")
+                print(f"{self.userNumber} doesn't exist int the list!\n")
                 print(f"Try another number!\n")
 
     # Seek bigger element
     def biggerElement(self):
-        return max(self.myList)
+        if not self.myList:
+            print("Empty list")
+            return
+        result = max(self.myList)
+        print(result)
 
     # Seek smaller element
     def smallerElement(self):
-        return min(self.myList)
+        if not self.myList:
+            print("Empty list")
+            return
+        result = min(self.myList)
+        print(result)
 
     # Average
     def listAverage(self):
         if not self.myList:
             print("Empty list")
             return
-        return sum(self.myList) / len(self.myList)
+        result = sum(self.myList) / len(self.myList)
+        print(result)
 
 
-testList = []
-myTest = ListManipulator(testList)
-myTest.addListElement()
-print(testList)
+class ManipulatorMenu:
+    def __init__(self, userList):
+        self.myList = userList
+        # Instance
+        self.myListManipulator = ListManipulator(self.myList)
+
+    def showMenu(self):
+        menu = """
+        ----- LIST MANIPULATOR -----\n
+        1. Add Element
+        2. Remove Element
+        3. Bigger Element
+        4. Smaller Element
+        5. Average
+        6. Show List
+        7. Exit
+        """
+        print(menu)
+
+    def menu(self):
+        while True:
+            self.showMenu()
+            userChoice = self.myListManipulator.checkNumber()
+            match userChoice:
+                case 1:
+                    self.myListManipulator.addListElement()
+                case 2:
+                    self.myListManipulator.removeElement()
+                case 3:
+                    self.myListManipulator.biggerElement()
+                case 4:
+                    self.myListManipulator.smallerElement()
+                case 5:
+                    self.myListManipulator.listAverage()
+                case 6:
+                    self.myListManipulator.showList()
+                case 7:
+                    print("\nExit\n")
+                    break
+                case _:
+                    print("\nDigit a valid option!\n")
+
+
+myList = []
+myManipulator = ManipulatorMenu(myList)
+myManipulator.menu()
